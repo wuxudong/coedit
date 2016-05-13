6c299659d8b544f43e4bf7a7468f7a05a4cef36a
wuxudong <wuxudong@wandoujia.comxudong82113@gmail.com>
2016-05-13 15:37:33 +0800

    code format

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -2,8 +2,6 @@
 
 /* Services */
 
-// Demonstrate how to register services
-// In this case it is a simple value service.
 angular
 	.module('myApp.services', []).value('version', '0.1')
 	.factory('ngstomp', function($rootScope) {
@@ -18,9 +16,7 @@ angular
         NGStomp.prototype.subscribe = function(queue, callback) {
             self.stompClient.subscribe(queue, function() {
                 var args = arguments;
-//                $rootScope.$apply(function() {
-                    callback(args[0]);
-//                });
+                callback(args[0]);
             });
         };
 
@@ -32,24 +28,18 @@ angular
         	self.stompClient.connect(headers,
                 function() {
         			var args = arguments;
-//                    $rootScope.$apply(function() {
-                        on_connect.call(undefined,args);
-//                    });
+                    on_connect.call(undefined,args);
                 },
                 function() {
                 	var args = arguments;
-//                    $rootScope.$apply(function() {
-                        on_error.apply(undefined,args);
-//                    });
+                    on_error.apply(undefined,args);
                 });
         };
 
         NGStomp.prototype.disconnect = function(callback) {
         	self.stompClient.disconnect(function() {
                 var args = arguments;
-//                $rootScope.$apply(function() {
-                    callback.apply(args);
-//                });
+                callback.apply(args);
             });
         };
 
